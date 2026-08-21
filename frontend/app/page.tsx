c44e88ed6112e37968c087d2c46914257084a35b
hezki-helios <yehezkiel.awan@helios.id>
2026-08-21 17:18:43 +0700

    demo test

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -41,7 +41,7 @@ export default async function DashboardPage() {
             </div>
             <div>
               <div className="font-semibold text-sm">Kouvénta Portal</div>
-              <div className="text-xs text-gray-500">Version 2.2 (Harbor Webhook Triggered)</div>
+              <div className="text-xs text-gray-500">Version 2.3 (demo test)</div>
             </div>
           </div>
 
